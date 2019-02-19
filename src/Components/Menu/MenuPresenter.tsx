20c43694b2c804dca2156934d1624fb124dc0812
jinyeong-jung <jinyeong32@gmail.com>
2019-02-19 13:11:51 +0900

    Updating Driver Mode (w/ refetchQueries)

--- src/Components/Menu/MenuPresenter.tsx
+++ src/Components/Menu/MenuPresenter.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { MutationFn } from "react-apollo";
 import { Link } from "react-router-dom";
 import styled from "../../typed-components";
-import { userProfile } from "../../types/api";
+import { toggleDriving, userProfile } from "../../types/api";
 
 const Container = styled.div`
   height: 100%;
@@ -77,11 +78,13 @@ const ToggleDriving = styled<IToggleProps, any>("button")`
 interface IProps {
   data?: userProfile;
   loading: boolean;
+  toggleDrivingFn: MutationFn<toggleDriving>;
 }
 
 const MenuPresenter: React.SFC<IProps> = ({
   data: { GetMyProfile: { user = null } = {} } = {},
-  loading
+  loading,
+  toggleDrivingFn
 }) => (
   <Container>
     {!loading && user && user.fullName && (
@@ -104,7 +107,7 @@ const MenuPresenter: React.SFC<IProps> = ({
         </Header>
         <SLink to="/trips">Your Trips</SLink>
         <SLink to="/settings">Settings</SLink>
-        <ToggleDriving isDriving={user.isDriving}>
+        <ToggleDriving isDriving={user.isDriving} onClick={toggleDrivingFn}>
           {user.isDriving ? "Stop driving" : "Start driving"}
         </ToggleDriving>
       </React.Fragment>
