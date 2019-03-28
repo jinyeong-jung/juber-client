cf035fa0d36e17d8338b66e680c2223021289436
jinyeong-jung <jinyeong32@gmail.com>
2019-03-28 18:14:03 +0900

    Redirect passenger and driver to Ride screen

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -144,7 +144,10 @@ class HomeContainer extends React.Component<IProps, IState> {
                         subscribeToMore(rideSubscriptionOptions);
                       }
                       return (
-                        <AcceptRide mutation={ACCEPT_RIDE}>
+                        <AcceptRide
+                          mutation={ACCEPT_RIDE}
+                          onCompleted={this.handleRideAcceptance}
+                        >
                           {acceptRideFn => (
                             <HomePresenter
                               loading={loading}
@@ -402,11 +405,20 @@ class HomeContainer extends React.Component<IProps, IState> {
     }
   };
   public handleRideRequest = (data: requestRide) => {
+    const { history } = this.props;
     const { RequestRide } = data;
     if (RequestRide.ok) {
       toast.success("Drive requested! Finding a driver..");
+      history.push(`/ride/${RequestRide.ride!.id}`);
     } else {
       toast.error(RequestRide.error);
+    }
+  };
+  public handleRideAcceptance = (data: acceptRide) => {
+    const { history } = this.props;
+    const { UpdateRideStatus } = data;
+    if (UpdateRideStatus.ok) {
+      history.push(`/ride/${UpdateRideStatus.rideId}`);
     }
   };
 }
--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -211,6 +211,7 @@ export interface acceptRide_UpdateRideStatus {
   __typename: "UpdateRideStatusResponse";
   ok: boolean;
   error: string | null;
+  rideId: number | null;
 }
 
 export interface acceptRide {
