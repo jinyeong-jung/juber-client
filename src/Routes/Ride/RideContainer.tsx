d01a35aac4adfee627cb2f4a0af81cac41da8617
jinyeong-jung <jinyeong32@gmail.com>
2019-03-29 17:01:35 +0900

    Get Ride Query

--- src/Routes/Ride/RideContainer.tsx
+++ src/Routes/Ride/RideContainer.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
+import { USER_PROFILE } from "src/sharedQueries";
+import { getRide, getRideVariables, userProfile } from "src/types/api";
 import RidePresenter from "./RidePresenter";
+import { GET_RIDE } from "./RideQueries";
 
-class RideContainer extends React.Component {
+class RideQuery extends Query<getRide, getRideVariables> {}
+class ProfileQuery extends Query<userProfile> {}
+
+interface IProps extends RouteComponentProps<any> {}
+
+class RideContainer extends React.Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+    if (!props.match.params.rideId) {
+      props.history.push("/");
+    }
+  }
   public render() {
-    return <RidePresenter />;
+    const {
+      match: {
+        params: { rideId }
+      }
+    } = this.props;
+    return (
+      <ProfileQuery query={USER_PROFILE}>
+        {({ data: userData }) => (
+          <RideQuery query={GET_RIDE} variables={{ rideId: Number(rideId) }}>
+            {({ data, loading }) => (
+              <RidePresenter
+                data={data}
+                userData={userData}
+                loading={loading}
+              />
+            )}
+          </RideQuery>
+        )}
+      </ProfileQuery>
+    );
   }
 }
 
