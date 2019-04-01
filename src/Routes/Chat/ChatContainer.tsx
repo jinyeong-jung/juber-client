84cff1de27349e21f47e8939ab52ca9d9b28e4fe
jinyeong-jung <jinyeong32@gmail.com>
2019-04-01 14:11:09 +0900

    Chat Screen Mutation

--- src/Routes/Chat/ChatContainer.tsx
+++ src/Routes/Chat/ChatContainer.tsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { Mutation, MutationFn, Query } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
 import { USER_PROFILE } from "src/sharedQueries";
-import { getChat, userProfile } from "src/types/api";
+import {
+  getChat,
+  sendMessage,
+  sendMessageVariables,
+  userProfile
+} from "src/types/api";
 import ChatPresenter from "./ChatPresenter";
-import { GET_CHAT } from "./ChatQueries";
+import { GET_CHAT, SEND_MESSAGE } from "./ChatQueries";
 
 interface IProps extends RouteComponentProps<any> {}
+interface IState {
+  message: "";
+}
 
 class ChatQuery extends Query<getChat> {}
 class ProfileQuery extends Query<userProfile> {}
+class SendMessageMutation extends Mutation<sendMessage, sendMessageVariables> {}
 
-class ChatContainer extends React.Component<IProps> {
+class ChatContainer extends React.Component<IProps, IState> {
+  public sendMessageFn: MutationFn;
   constructor(props: IProps) {
     super(props);
     if (!props.match.params.chatId) {
       props.history.push("/");
     }
+    this.state = {
+      message: ""
+    };
   }
   public render() {
     const {
@@ -24,22 +37,60 @@ class ChatContainer extends React.Component<IProps> {
         params: { chatId }
       }
     } = this.props;
+    const { message } = this.state;
     return (
       <ProfileQuery query={USER_PROFILE}>
         {({ data: userData }) => (
           <ChatQuery query={GET_CHAT} variables={{ chatId: Number(chatId) }}>
             {({ data, loading }) => (
-              <ChatPresenter
-                data={data}
-                loading={loading}
-                userData={userData}
-              />
+              <SendMessageMutation mutation={SEND_MESSAGE}>
+                {sendMessageFn => {
+                  this.sendMessageFn = sendMessageFn;
+                  return (
+                    <ChatPresenter
+                      data={data}
+                      loading={loading}
+                      userData={userData}
+                      messageText={message}
+                      onInputChange={this.onInputChange}
+                      onSubmit={this.onSubmit}
+                    />
+                  );
+                }}
+              </SendMessageMutation>
             )}
           </ChatQuery>
         )}
       </ProfileQuery>
     );
   }
+  public onInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
+  public onSubmit = () => {
+    const { message } = this.state;
+    const {
+      match: {
+        params: { chatId }
+      }
+    } = this.props;
+    if (message !== "") {
+      this.setState({
+        message: ""
+      });
+      this.sendMessageFn({
+        variables: {
+          chatId: Number(chatId),
+          text: message
+        }
+      });
+    }
+  };
 }
 
 export default ChatContainer;
