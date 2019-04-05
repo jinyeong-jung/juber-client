72815bac3fbd120ac9e01ac1dfa06aae1d6b8b00
jinyeong-jung <jinyeong32@gmail.com>
2019-04-05 18:58:58 +0900

    Redirect to home after update driving mode

--- src/Components/Menu/MenuContainer.tsx
+++ src/Components/Menu/MenuContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Mutation, Query } from "react-apollo";
+import { toast } from "react-toastify";
 import { USER_PROFILE } from "../../sharedQueries";
 import { toggleDriving, userProfile } from "../../types/api";
 import MenuPresenter from "./MenuPresenter";
@@ -15,6 +16,17 @@ class MenuContainer extends React.Component {
       <ToggleDrivingMutation
         mutation={TOGGLE_DRIVING}
         refetchQueries={[{ query: USER_PROFILE }]}
+        onCompleted={data => {
+          const { ToggleDrivingMode } = data;
+          if (ToggleDrivingMode.ok) {
+            toast.success("Updated!");
+            setTimeout(() => {
+              window.location.href = "/juber-client";
+            }, 2000);
+          } else if (ToggleDrivingMode.error) {
+            toast.error(ToggleDrivingMode.error);
+          }
+        }}
       >
         {toggleDrivingFn => (
           <ProfileQuery query={USER_PROFILE}>
